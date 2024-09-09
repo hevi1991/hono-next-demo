4574abb180e5b21f1313021742e9aea82b4618f0
msme-heww <he.wei@email.cn>
2024-09-09 14:11:38 +0800

    schedule tasks

--- src/cron.mjs
+++ src/cron.mjs
@@ -1,11 +1,19 @@
 import cron from "node-cron";
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
 
 // 定时任务
 
-cron.schedule("*/5 * * * * *", () => {
+cron.schedule("*/5 * * * * *", async () => {
   // 定时任务逻辑
   console.log("定时任务执行了！", Date.now());
 });
 
+cron.schedule("*/5 * * * * *", async () => {
+  // 定时任务逻辑
+  const count = await db.runLog.count();
+  console.log("日志数：", count);
+});
 
 export default cron;
